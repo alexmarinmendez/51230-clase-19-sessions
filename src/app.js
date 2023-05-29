6fbf22fa744707bbc849a30a1f71f041946b4f2a
Alex Marin Mendez <alexmarinmendez@gmail.com>
2023-05-29 08:47:13 -0500

    feat: add file store session

--- src/app.js
+++ src/app.js
@@ -1,16 +1,22 @@
 import express from 'express'
 import session from 'express-session'
+import FileStore from 'session-file-store'
 
 const app = express()
+const fileStore = FileStore(session)
 
 app.use(session({
+    store: new fileStore({
+        path: './sessions',
+        ttl: 120,
+        retries: 2
+    }),
     secret: 'c0d3r',
     resave: true,
     saveUninitialized: true
 }))
 
 app.get('/', (req, res) => {
-    console.log(req.session)
     res.send('ok')
 })
 
@@ -18,18 +24,15 @@ app.get('/login', (req, res) => {
     const { username } = req.query
     if (username == '') return res.send('Need a username')
     req.session.user = username
-    console.log(req.session)
     res.send('Login success!')
 })
 
 app.get('/logout', (req, res) => { 
-    req.session.destroy(err => res.send(err))
-    console.log(req.session)
+    req.session.destroy(err => console.log(err))
     res.send('Logout success!')
 })
 
 app.get('/private', (req, res) => {
-    console.log(req.session)
     if (req.session.user && req.session.user==='alex') return res.send('Private page!')
     res.send('No Permissions')
 })
